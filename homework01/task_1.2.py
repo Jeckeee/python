7b21861d08e937a2d772a2adb4e84ccc405021e7
Jeckeee <64081308+Jeckeee@users.noreply.github.com>
2022-05-17 16:37:31 +0700

    Update task_1.2.py

--- homework01/task_1.2.py
+++ homework01/task_1.2.py
@@ -1,16 +1,30 @@
 import time
 
 # Вариант 1
-time_second = int(input("Введите время в секундах >>> "))
+time_second = 3849  # int(input("Введите время в секундах >>> "))
 time_format = time.strftime("%H:%M:%S", time.gmtime(time_second))
 
 print(time_format)
 
-
 # Вариант 2
 
-time_sec = time_second % 60
-time_min = (time_second - time_sec) % 3600 // 60
 time_hour = time_second // 3600
+time_min = time_second % 3600 // 60
+time_sec = time_second % 60
 
-print(f'{time_hour}:{time_min}:{time_sec}')
+if time_hour < 10:
+    time_result = f'0{time_hour}:'
+else:
+    time_result = f'{time_hour}:'
+
+if time_min < 10:
+    time_result += f'0{time_min}:'
+else:
+    time_result += f'{time_min}:'
+
+if time_sec < 10:
+    time_result += f'0{time_sec}'
+else:
+    time_result += f'{time_sec}'
+
+print(time_result)
