5e26c426894e43f73646406221803b14f513a621
Jeckeee <64081308+Jeckeee@users.noreply.github.com>
2022-05-17 17:50:28 +0700

    Update task_1.4.py

--- homework01/task_1.4.py
+++ homework01/task_1.4.py
@@ -1,10 +1,24 @@
-n = int(input("Введите любое число >>> "))
+num = int(input("Введите любое число >>> "))
 
+# Version 1
+n = num
 ls = []
 
 while n > 0:
     ls.append(n % 10)
     n //= 10
 
-r = max(ls)
-print(r)
+max_num = max(ls)
+print(f'Version 1 - {max_num}')
+
+
+# Version 2
+n2 = num
+max_num = 0
+while n2 > 0:
+    num = n2 % 10
+    if num > max_num:
+        max_num = num
+    n2 //= 10
+
+print(f'Version 2 - {max_num}')
