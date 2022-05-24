42134efdd1ac4364fcf9c77559e89b7a66cb0ee2
Jeckeee <64081308+Jeckeee@users.noreply.github.com>
2022-05-24 11:45:11 +0700

    Update task-2-5.py

--- homework02/task-2-5.py
+++ homework02/task-2-5.py
@@ -6,23 +6,27 @@
 """
 
 string_input = "Пополнить рейтинг (да) или (нет)? >>> "
-list_rating = [7, 5, 3, 3, 2, 8]
+list_rating = [7, 7, 5, 3, 3, 2]
 
 print(f"Рейтинг: {list_rating}")
 append_elem = input(string_input)
 
 
 while True:
-
     if append_elem.lower() == 'да':
         new_number = int(input("Введите число >>> "))
-        list_rating.append(new_number)
-        list_rating.sort()
-        list_rating.reverse()
+        counter = 0
+        for el in list_rating:
+            if new_number <= el:
+                counter += 1
+            else:
+                break
+        list_rating.insert(counter, new_number)
         print(f"Новый рейтинг: {list_rating}")
         append_elem = input("Пополнить рейтинг? >>> ")
     elif append_elem.lower() == 'нет':
-        print(f"Ввод рейтинга закончен! Рейтинг: {list_rating}")
+        print("Ввод рейтинга закончен!")
+        print(list_rating)
         break
     else:
         print("Ошибка ввода. Введите значение (да) или (нет)!")
